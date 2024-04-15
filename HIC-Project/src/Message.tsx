90f0563447cb0d5647bd8afd59959c78eb8c7aeb
Lennice Bolton <lennicebolton03@gmail.com>
2024-04-15 17:35:19 -0400

    Navbar in progress/added bootstrap

---
+++ HIC-Project/src/Message.tsx
@@ -0,0 +1,17 @@
+
+
+// PascalCasing
+
+function Message(){
+    // JSX: JS xml
+    const name = 'TotallyRealCarsales.com ';
+    if(name)
+        return <h1>{name}</h1>;
+    return <h1>Hello World</h1>
+}
+
+
+
+
+
+export default Message;
