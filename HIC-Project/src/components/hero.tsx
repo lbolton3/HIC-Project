78b47bd393ce10dcec78110b05f72faa506b6210
Lennice Bolton <lennicebolton03@gmail.com>
2024-04-18 17:17:30 -0400

    worked on the hero section of the home page -added a cool car photo(tried to add a video but that didn't work will probs try again later)

---
+++ HIC-Project/src/components/hero.tsx
@@ -0,0 +1,16 @@
+import './hero.css'
+
+const HeroSection = () => {
+    return (
+        <section className="hero">
+            <div className="hero-content">
+                <h1>Welcome to Our Website</h1>
+                <p>Discover amazing products that will change your life.</p>
+                <button className="cta-button">Explore Now</button>
+            </div>
+        </section>
+    );
+};
+
+export default HeroSection;
+
--- HIC-Project/src/pages/Home.tsx
+++ HIC-Project/src/pages/Home.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import Header from '../components/Header';
-
+import HeroSection from '../components/hero';
 const Home: React.FC = () => {
-    return (
-      <>
-        <Header />
-        <div style={{ paddingTop: '60px', textAlign: 'center' }}> {/* Adjust the padding as necessary */}
-          <h1>Home</h1>
-          <p>Welcome to the home page!</p>
-        </div>
-      </>
-    );
-  };
+  return (
+    <>
+      <Header />
+      <HeroSection />
+      <div style={{ paddingTop: '60px', textAlign: 'center' }}> {/* Adjust the padding as necessary */}
+        <h1>Home</h1>
+        <p>Welcome to the home page!</p>
+      </div>
+    </>
+  );
+};
 
 export default Home;
