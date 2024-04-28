43acb8520e6ffe2223c024531a8f36f94a52dea7
Lennice Bolton <lennicebolton03@gmail.com>
2024-04-28 14:50:29 -0400

    fixed images
    fixed images

--- HIC-Project/src/components/loginHero.tsx
+++ HIC-Project/src/components/loginHero.tsx
@@ -3,9 +3,17 @@ import './login.css'
 const LoginSection = () => {
     return (
         <section className="login">
-            <div className="login-content">
-            </div>
-        </section>
+                <div className="login-content">
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlInput2" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="exampleFormControlInput2" placeholder="Enter your password" />
+                    </div>
+                </div>
+            </section>
     );
 };
 
--- HIC-Project/src/pages/login.tsx
+++ HIC-Project/src/pages/login.tsx
@@ -8,18 +8,6 @@ const Login: React.FC = () => {
         <>
             <Header />
             <LoginSection />
-            <section className="login">
-                <div className="login-content">
-                    <div className="mb-3">
-                        <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="exampleFormControlInput2" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="exampleFormControlInput2" placeholder="Enter your password" />
-                    </div>
-                </div>
-            </section>
         </>
     );
 };
