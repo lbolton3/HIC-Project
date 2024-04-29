9a3b2086b78eb2e50f34babb480db6359ee8af06
Bluehippo321 <149278207+Bluehippo321@users.noreply.github.com>
2024-04-28 20:05:30 -0400

    Update login.tsx

--- HIC-Project/src/pages/login.tsx
+++ HIC-Project/src/pages/login.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 import '../components/loginHero'
 import LoginSection from '../components/loginHero';
 
@@ -8,10 +9,7 @@ const login: React.FC = () => {
         <>
             <Header />
             <LoginSection />
-            <div style={{ paddingTop: '60px', textAlign: 'center' }}> {/* Adjust the padding as necessary */}
-                <h1>Login</h1>
-                <p>Welcome to the login page!</p>
-            </div>
+            <Footer />
         </>
     );
 };
