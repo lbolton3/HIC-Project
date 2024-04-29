a9370347fb69d371fd29e79e935cde26526f4184
Bluehippo321 <149278207+Bluehippo321@users.noreply.github.com>
2024-04-28 20:02:36 -0400

    Update loginHero.tsx

--- HIC-Project/src/components/loginHero.tsx
+++ HIC-Project/src/components/loginHero.tsx
@@ -1,13 +1,24 @@
+import { useNavigate } from 'react-router-dom';
 import './login.css'
 
 const LoginSection = () => {
+    const navigate = useNavigate();
     return (
+        
         <section className="login">
-            <div className="login-content">
-                <h1>Welcome to Our Website</h1>
-                <p>Discover amazing products that will change your life.</p>
-            </div>
-        </section>
+                <div className="login-content">
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+                        <form onSubmit = {()=>navigate("/account")}>
+                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                        </form>
+                    </div>
+                    {/* <div className="mb-3">
+                        <label htmlFor="exampleFormControlInput2" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="exampleFormControlInput2" placeholder="Enter your password" />
+                    </div> */}
+                </div>
+            </section>
     );
 };
 
